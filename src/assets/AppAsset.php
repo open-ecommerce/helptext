4b7d9f7f77cec6024cc83caf87396cf8e9ae53b1
Eduardo Silva <eduardo@open-ecommerce.org>
2016-07-06 15:05:45 +0100

    changing web index

--- src/assets/AppAsset.php
+++ src/assets/AppAsset.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\assets;
 
+use yii\helpers\FileHelper;
 use yii\web\AssetBundle;
 
 class AppAsset extends AssetBundle
@@ -8,10 +9,10 @@ class AppAsset extends AssetBundle
     //public $sourcePath = '@app/assets/dist';
     public $sourcePath = '@app/assets/web/dist';
     public $css = [
-        YII_ENV_DEV ? 'css/app.css' : 'css/app.min.css'
+        YII_ENV_DEV ? 'css/app.css' : 'css/app.min.css',
     ];
     public $js = [
-        YII_ENV_DEV ? 'js/app.js' : 'js/app.min.js'
+        YII_ENV_DEV ? 'js/app.js' : 'js/app.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
