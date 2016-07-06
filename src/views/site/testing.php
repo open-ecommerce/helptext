a722ac93d136822e4ba76fef572613d936d97255
Eduardo Silva <eduardo@open-ecommerce.org>
2016-07-06 17:57:43 +0100

    quick json to pull with composer

--- src/views/site/testing.php
+++ src/views/site/testing.php
@@ -15,8 +15,11 @@ $this->params['breadcrumbs'][] = $this->title;
         $message = $twilioService->account->messages->create(array(
             "From" => "+441234480212", // From a valid Twilio number
             "To" => "+447551524625",   // Text this number
-            "Body" => "este es un mensage del sistema usando twillo seria bueno que funque y que viva la revolucion!!!!!!!!!",
+            "Body" => "esto es del live",
         ));
+        
+        echo "se habra mandado el texto?";
+        
     } catch (\Services_Twilio_RestException $e) {
             echo $e->getMessage();
     }
