6771a01aa3c9b769bbbc7334f55bcd639754966d
eduardo <eduardo@open-ecommerce.org>
2016-07-24 17:26:00 +0100

    changed ignore and added backend

--- src/modules/backend/views/default/index.php
+++ src/modules/backend/views/default/index.php
@@ -1,3 +1,7 @@
+<?php
+use dosamigos\chartjs\ChartJs;
+?>
+
 <!-- check documentation https://almsaeedstudio.com/themes/AdminLTE/documentation/index.html#introduction -->
 <div class="row">
     <div class="col-md-3 col-xs-6">
@@ -123,6 +127,47 @@
 </div><!-- /.info-box -->
 </div>
 
+
+<div class="row">
+
+
+
+      <?= ChartJs::widget([
+          'type' => 'line',
+          'options' => [
+              'height' => 100,
+              'width' => 400
+          ],
+          'data' => [
+              'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+              'datasets' => [
+                  [
+                      'fillColor' => "rgba(220,220,220,0.5)",
+                      'strokeColor' => "rgba(220,220,220,1)",
+                      'pointColor' => "rgba(220,220,220,1)",
+                      'pointStrokeColor' => "#fff",
+                      'data' => [65, 59, 90, 81, 56, 55, 40]
+                  ],
+                  [
+                      'fillColor' => "rgba(151,187,205,0.5)",
+                      'strokeColor' => "rgba(151,187,205,1)",
+                      'pointColor' => "rgba(151,187,205,1)",
+                      'pointStrokeColor' => "#fff",
+                      'data' => [28, 48, 40, 19, 96, 27, 100]
+                  ]
+              ]
+          ]
+      ]);
+      ?>
+
+
+</div>
+
+
+
+
+
+
 <div class="row">
 
 <!-- Construct the box with style you want. Here we are using box-danger -->
@@ -196,46 +241,7 @@
 </div><!--/.direct-chat -->
 </div>
 
-<canvas id="myChart" width="400" height="400"></canvas>
-<script>
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        }
-    }
-});
-</script>
+
 
 
 <?= \dmstr\modules\prototype\widgets\HtmlWidget::widget(['enableFlash' => true]); ?>
