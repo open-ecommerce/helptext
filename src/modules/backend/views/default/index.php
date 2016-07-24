b8511b4b7fc4d50b1df95d4afda49814f1fc4a34
eduardo <eduardo@open-ecommerce.org>
2016-07-24 16:56:56 +0100

    changed ignore and added backend

--- src/modules/backend/views/default/index.php
+++ src/modules/backend/views/default/index.php
@@ -1,3 +1,4 @@
+<!-- check documentation https://almsaeedstudio.com/themes/AdminLTE/documentation/index.html#introduction -->
 <div class="row">
     <div class="col-md-3 col-xs-6">
         <!-- small box -->
@@ -32,7 +33,7 @@
                     </h3>
 
                     <p>
-                        Users
+                        Users ja ja
                     </p>
                 </div>
                 <div class="icon">
@@ -92,5 +93,149 @@
     <?php endif; ?>
 
 </div>
+
+<div class="row">
+    <div class="info-box">
+      <!-- Apply any bg-* class to to the icon to color it -->
+      <span class="info-box-icon bg-red"><i class="fa fa-star-o"></i></span>
+      <div class="info-box-content">
+        <span class="info-box-text">Likes esto viene del index de /var/www/helptext/src/modules/backend/views/default/index.php</span>
+        <span class="info-box-number">93,139</span>
+      </div><!-- /.info-box-content -->
+    </div><!-- /.info-box -->
+</div>
+
+<div class="row">
+<!-- Apply any bg-* class to to the info-box to color it -->
+<div class="info-box bg-red">
+  <span class="info-box-icon"><i class="fa fa-comments-o"></i></span>
+  <div class="info-box-content">
+    <span class="info-box-text">Likes</span>
+    <span class="info-box-number">41,410</span>
+    <!-- The progress section is optional -->
+    <div class="progress">
+      <div class="progress-bar" style="width: 70%"></div>
+    </div>
+    <span class="progress-description">
+      70% Increase in 30 Days
+    </span>
+  </div><!-- /.info-box-content -->
+</div><!-- /.info-box -->
+</div>
+
+<div class="row">
+
+<!-- Construct the box with style you want. Here we are using box-danger -->
+<!-- Then add the class direct-chat and choose the direct-chat-* contexual class -->
+<!-- The contextual class should match the box, so we are using direct-chat-danger -->
+<div class="box box-danger direct-chat direct-chat-danger">
+  <div class="box-header with-border">
+    <h3 class="box-title">Direct Chat</h3>
+    <div class="box-tools pull-right">
+      <span data-toggle="tooltip" title="3 New Messages" class="badge bg-red">3</span>
+      <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+      <!-- In box-tools add this button if you intend to use the contacts pane -->
+      <button class="btn btn-box-tool" data-toggle="tooltip" title="Contacts" data-widget="chat-pane-toggle"><i class="fa fa-comments"></i></button>
+      <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+    </div>
+  </div><!-- /.box-header -->
+  <div class="box-body">
+    <!-- Conversations are loaded here -->
+    <div class="direct-chat-messages">
+      <!-- Message. Default to the left -->
+      <div class="direct-chat-msg">
+        <div class="direct-chat-info clearfix">
+          <span class="direct-chat-name pull-left">Alexander Pierce</span>
+          <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>
+        </div><!-- /.direct-chat-info -->
+        <img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="message user image"><!-- /.direct-chat-img -->
+        <div class="direct-chat-text">
+          Is this template really for free? That's unbelievable!
+        </div><!-- /.direct-chat-text -->
+      </div><!-- /.direct-chat-msg -->
+
+      <!-- Message to the right -->
+      <div class="direct-chat-msg right">
+        <div class="direct-chat-info clearfix">
+          <span class="direct-chat-name pull-right">Sarah Bullock</span>
+          <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>
+        </div><!-- /.direct-chat-info -->
+        <img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="message user image"><!-- /.direct-chat-img -->
+        <div class="direct-chat-text">
+          You better believe it!
+        </div><!-- /.direct-chat-text -->
+      </div><!-- /.direct-chat-msg -->
+    </div><!--/.direct-chat-messages-->
+
+    <!-- Contacts are loaded here -->
+    <div class="direct-chat-contacts">
+      <ul class="contacts-list">
+        <li>
+          <a href="#">
+            <img class="contacts-list-img" src="../dist/img/user1-128x128.jpg" alt="Contact Avatar">
+            <div class="contacts-list-info">
+              <span class="contacts-list-name">
+                Count Dracula
+                <small class="contacts-list-date pull-right">2/28/2015</small>
+              </span>
+              <span class="contacts-list-msg">How have you been? I was...</span>
+            </div><!-- /.contacts-list-info -->
+          </a>
+        </li><!-- End Contact Item -->
+      </ul><!-- /.contatcts-list -->
+    </div><!-- /.direct-chat-pane -->
+  </div><!-- /.box-body -->
+  <div class="box-footer">
+    <div class="input-group">
+      <input type="text" name="message" placeholder="Type Message ..." class="form-control">
+      <span class="input-group-btn">
+        <button type="button" class="btn btn-danger btn-flat">Send</button>
+      </span>
+    </div>
+  </div><!-- /.box-footer-->
+</div><!--/.direct-chat -->
+</div>
+
+<canvas id="myChart" width="400" height="400"></canvas>
+<script>
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+</script>
+
 
 <?= \dmstr\modules\prototype\widgets\HtmlWidget::widget(['enableFlash' => true]); ?>
--- src/views/site/testing.php
+++ src/views/site/testing.php
@@ -1,5 +1,6 @@
 <?php
 use yii\helpers\Html;
+use dosamigos\chartjs\ChartJs;
 
 /* @var $this yii\web\View */
 $this->title                   = 'About this system';
@@ -7,23 +8,52 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="site-about">
+
+aca esta el testeo:
+
+
 <?php
 
-    $twilioService = Yii::$app->Yii2Twilio->initTwilio();
+$settings = Yii::$app->settings;
 
-    try {
-        $message = $twilioService->account->messages->create(array(
-            "From" => "+441234480212", // From a valid Twilio number
-            "To" => "+447551524625",   // Text this number
-            "Body" => "esto es del live",
-        ));
-        
-        echo "se habra mandado el texto?";
-        
-    } catch (\Services_Twilio_RestException $e) {
-            echo $e->getMessage();
-    }
+echo "y el setting es: " . $settings->get('sms', 'test');
 
-?>    
-    
+?>
+
+<div class="row">
+
+
+  <?= ChartJs::widget([
+      'type' => 'line',
+      'options' => [
+          'height' => 400,
+          'width' => 400
+      ],
+      'data' => [
+          'labels' => ["January", "February", "March", "April", "May", "June", "July"],
+          'datasets' => [
+              [
+                  'fillColor' => "rgba(220,220,220,0.5)",
+                  'strokeColor' => "rgba(220,220,220,1)",
+                  'pointColor' => "rgba(220,220,220,1)",
+                  'pointStrokeColor' => "#fff",
+                  'data' => [65, 59, 90, 81, 56, 55, 40]
+              ],
+              [
+                  'fillColor' => "rgba(151,187,205,0.5)",
+                  'strokeColor' => "rgba(151,187,205,1)",
+                  'pointColor' => "rgba(151,187,205,1)",
+                  'pointStrokeColor' => "#fff",
+                  'data' => [28, 48, 40, 19, 96, 27, 100]
+              ]
+          ]
+      ]
+  ]);
+  ?>
+
+
+</div>
+
+
+
 </div>
