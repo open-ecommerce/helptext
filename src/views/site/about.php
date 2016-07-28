0342c91851f967337975cf6a33dad2e31bc898e5
Mariano Marey <mariano@open-ecommerce.org>
2016-07-28 11:57:04 +0100

    cambios home

--- src/views/site/about.php
+++ src/views/site/about.php
@@ -1,16 +1,34 @@
 <?php
+use app\assets\AppAsset;
+use app\widgets\Alert;
 use yii\helpers\Html;
 
+
+$bundle = AppAsset::register($this);
+$imgPath = $bundle->baseUrl;
+
 /* @var $this yii\web\View */
-$this->title                   = 'About this system';
+$this->title                   = 'About Us';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 
 <div class="site-about">
-    
-    esto seria esto  el about us 
-    
-   
-                
+  <div class="col-md-12">
+    <h2>We are open ecommerce</h2>
+    <p>We are a group of independent developers, marketing researchers & designers specialized in Open Source platforms as Magento, Yii and Wordpress working from our studio in London.</p>
 
+    <p>Open e-commerce was set up as a social enterprise. Our projects are united by a common underlying idea. Everything we do aims to empower people and go along them in the path to realize their projects.</p>
+
+    <p>Part of the income from our Agency work goes to create links with the community and develop community projects. So far we have engaged with projects in South America, and more recently European and UK-based projects. This is our philosophy.</p>
+  </div>
+    <div class="col-md-6">
+      <h3> How can we help you?</h3>
+      <p>If you are thinking of adding text to your helpline, please contact us.</p>
+      <p> We will help you with our expertise and build a system to cater for your needs.</p>
+      <p>Send us an email to <a href="mailto:info@helptext.uk?Subject=Landing Page" target="_top">info@helptext.uk</a> or text us to 07879387106, and we will get back to you within 24 hours.</p>
+    </br>
+    </div>
+    <div class="col-md-6">
+      <?= Html::img($imgPath.'/img/helptext-offices_4.jpg', ['alt' => 'Open-ecommerce.org Office'])?>
+    </div>
 </div>
--- src/views/site/index.php
+++ src/views/site/index.php
@@ -1,28 +1,48 @@
 <?php
 /* @var $this yii\web\View */
 
+use app\assets\AppAsset;
+use app\widgets\Alert;
 use yii\helpers\Html;
+
+$bundle = AppAsset::register($this);
+$imgPath = $bundle->baseUrl;
 
 $this->title = 'TextHelp+ for NNLS Drop-in';
 ?>
 
-<div class="site-index ">
-       
-<br>    
-       
-<br>         
-<br>         
-<br>         
-<br>         
-<br>         
-<br>         
-<br>         
-<br>      
-    
-    
-    aca estamos en el index
-    
-    
+<div class="site-index container">
+  <div class="row">
+  </br>
+  </br>
+  </br>
+    <h3>Our Messaging Platform</h3>
+    <p>HELPtext+ messaging platform - in development - allows the integration of text and multimedia messages, coming from various sources (e.g. text, Facebook Messenger) onto a single interface.</p>
 
-</div>    
-    
+    <p>Our mission is to link-up helpers and people in crisis. HELPtext+  will equip helplines with a tool to open emerging communication channels easily and will provide them with the data to manage their resources more effectively, turning new and existing helplines into digital communicators.</p>
+
+    <p>The asynchronous nature of messages - as opposed to calls - allows a new service delivery model where accurate prioritising and management of cases increase helpers' ability to scale up their impact. </p>
+
+    <p>Our system reduces the cost of the service, by allowing helpers to respond to calls wherever they are, on web or app, with an automated rule or a supervisor allocating cases. This avoids the costs associated with call centres and enables transnational support without the additional expense of long distance calls.</p>
+
+    <p>Impact is measured via automated reports, giving frequency, time and day of cases, all segmented by types, which are tailored to the specific service provided by each helpline. This valuable data aims at generating a deep insight into the behaviour and needs of users, enabling helplines to recruit and manage helpers more adequately and, thus, maximise their impact.</p>
+
+  </div>
+  <div class="row">
+        <div class="col-lg-10 col-lg-offset-2">
+        <?= Html::img($imgPath.'/img/helper_on_phone_3.jpg', ['alt' => 'Helptext Helpline Solutions'])?>
+  </div>
+  <div class="row">
+    <h3>Is Your Helpline Ready?</h3>
+    <p>Your users are changing: 32% of people would rather text you than talk to you, and 51% of teens would rather communicate digitally than in person, even with friends.</p>
+
+    <p>Messaging allows you to reach to people that are too vulnerable to talk, for fear of being heard, such as homeless women or domestic violence victims. Adults on the autistic spectrum prefer text to talk and people with English as a 2nd language find it easier to understand a text than a person on the phone.</p>
+
+    <p>HLPText+ is a platform designed exclusively to empower non-profit helplines like yours. You will be able to easily set up free text line and provide safe, secure, and confidential support via SMS. We provide you with a simple web interface so that you can manage these texts like emails, on your phone or a computer.</p>
+
+    <p>Whether you have many case-workers or just a couple, full time or at specific times of the day, the platform will help you manage your users' expectations.</p>
+
+    <p>And you have our commitment that we will upgrade the system continuously, to enable your helpline to keep up with your users. So that you can keep doing what you do best: HELP.</p>
+
+  </div>
+</div>
